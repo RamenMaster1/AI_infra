ef3c23dc2e05321d5cbcf4f28ca0cc3dff56de42
ZeroSeraph <zero20030403@gmail.com>
2025-12-08 16:02:39 +0800

    softmax_v2,在softmax_v1的基础上实现warp shuffle

--- kernel_learn/softmax/readme.md
+++ kernel_learn/softmax/readme.md
@@ -12,3 +12,64 @@ v1：
     3. 块内规约，见v1.png
     4. 对于从global->shared mmeory，就用向量化取值来减少访存次数
     5. 注意，只是为了实现这个softmax，所以数据设置的比较少，并没有要去适配好多形状的矩阵
+
+v2:
+    v1版本的块内规约，在for(int k=BLOCK_SIZE/2;k>0;k/=2)部分效率实在太低（每一次还有一个__syncthreads(),因为一个线程需要使用到其它线程在上一轮的结果，所以需要上一轮的线程将结果写入到shared memory中，才能开始下一轮的操作）。但是使用warp shuffle可以进行优化。
+    warp shuffle:是一类作用于 线程束（warp）内部 的协同操作函数，其主要目的是允许同一个 warp 中的不同线程直接访问彼此的寄存器数据，从而提供一种高效的信息交换机制，（不用等到写回到shared memory中了）
+
+
+关于warp shuflle操作，最重要的还是__shfl_down_sync函数，下面是warp shuffle的知识点：
+1. warp shuffle = warp 内线程之间通过寄存器直接交换数据的机制。
+    特点：
+        只能在同一个 warp（32 线程）内部通信
+        无需共享内存
+        无需 __syncthreads()
+        延迟极低（几乎就是一次寄存器 read）
+    用途：
+        求和（sum reduction）
+        求最大/最小值（max/min reduction）
+        扫描（scan）
+        warp 内数据重排、广播
+2. 为什么 warp shuffle 比共享内存快？
+    传统方式：shared memory 读写 + __syncthreads()
+        每一步都需要读写共享内存（慢）
+        每一层循环都得 __syncthreads()（更慢）
+    warp shuffle：
+        所有操作都在寄存器内完成
+        硬件保证 32 线程同步，不需要 __syncthreads()
+        没有共享内存访问
+        减少指令数量
+3. 最重要的函数__shfl_down_sync(mask, v, offset)
+    1. mask：表示哪些线程需要参与运算，无脑是0xffffffff，表示所有线程都参与运算
+    2. v：本线程当前手里的值（会broadcast到所有thread，然后其它thread再判断自己是否需要这个值）。
+    3. offset：当前线程需要从哪一个偏移量的下一个线程获取值（当前线程的索引 + offset）
+    4. 函数返回值：获取的第三步的偏移位置的线程对应的值
+4. 小例子：
+    假设 warp 内 8 个线程（简化）持有：
+        lane:    0  1  2  3  4  5  6  7
+        v:       10 20 30 40 50 60 70 80
+    __shfl_down_sync(0xffffffff, v[i], 2):
+        lane0 -> 拿 lane2 的 v = 30
+        lane1 -> 拿 lane3 的 v = 40
+        lane2 -> 拿 lane4 的 v = 50
+        lane3 -> 拿 lane5 的 v = 60
+        lane4 -> 拿 lane6 的 v = 70
+        lane5 -> 拿 lane7 的 v = 80
+        lane6 -> 拿 lane8（不存在）
+        lane7 -> 拿 lane9（不存在）
+    最终：
+        返回值：30 40 50 60 70 80 ?? ??
+    
+    就这样可以解决最大值、sum的规约问题：
+
+5. max/min reduce：
+
+    for (int offset = 16; offset > 0; offset /= 2) {
+        tmp_max_value[tid]=max(tmp_max_value[tid],__shfl_down_sync(0xffffffff, tmp_max_value[tid], offset));
+    }
+    
+6. sum reduce：
+
+    for(int offset=16;offset>0;offset/=2){
+        tmp_sum_value[tid]=tmp_sum_value[tid]+__shfl_down_sync(0xffffffff,tmp_sum_value[tid],offset);
+    }
---
+++ kernel_learn/softmax/softmax_v2.cu
@@ -0,0 +1,169 @@
+# include <cmath>
+#include <cstdio>
+
+#define FETCH_FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
+void matrix_init(float* matrix,int M,int N){
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            matrix[i*N+j]=(float)rand()/RAND_MAX;
+        }    
+    }
+}
+
+void softmax_cpu(float* matrix,int M,int N){ 
+    // 依次对每一行操作
+    for(int i=0;i<M;i++){
+        // 获取每行最大值
+        float max_value=-INFINITY;
+        for(int j=0;j<N;j++){
+            max_value=max(max_value,matrix[i*N+j]);
+        }
+
+        // 防止上溢，并计算出指数化的sum结果
+        float sum_value=0.0f;
+        for(int k=0;k<N;k++){
+            sum_value=sum_value+exp(matrix[i*N+k]-max_value);
+        }
+
+        // 计算softmax
+        for(int l=0;l<N;l++){
+            matrix[i*N+l]=exp(matrix[i*N+l]-max_value)/sum_value;   
+        }
+    }
+}
+
+void compare_matrix(int m, int n, float* A, float* B) {
+    const float epsilon = 1e-5f; 
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            float diff = fabsf((A[i * n + j] - B[i * n + j]))/A[i * n + j];
+            if (diff > epsilon) {
+                printf("error at (%d,%d): A=%f, B=%f, diff=%f\n", i, j, A[i * n + j], B[i * n + j], diff);
+                return;
+            }
+        }
+    }
+    printf("Matrix comparison passed with epsilon=%f\n", epsilon);
+}
+
+template<int shared_size,int BLOCK_SIZE> //shared memory shape:512
+__global__ void softmax_gpu(float* matrix,int M,int N){
+
+    int tid=threadIdx.x;
+    float* matrix_start=matrix+blockIdx.y*N;
+    float max_value;
+    float sum_value;
+
+    __shared__ float s_matrix[shared_size];
+    __shared__ float tmp_max_value[BLOCK_SIZE];  // 暂存每一个线程的max_value，不要写到s_matrix里面，不然还得再从global读取
+    __shared__ float tmp_sum_value[BLOCK_SIZE];
+    tmp_max_value[tid]=-INFINITY;  // 每一个线程都会对自己对应的tmp_max_value进行初始化
+    tmp_max_value[tid]=0.0f;
+
+    // 64个线程，那么每个线程处理8个数据，每个线程做两次向量化取值
+    for(int i=0;i<N/blockDim.x/4;i++){
+        float4 temp = FETCH_FLOAT4(matrix_start[i*blockDim.x*4+tid*4]);
+        s_matrix[i*blockDim.x+tid*4]=temp.x;
+        s_matrix[i*blockDim.x+tid*4+1]=temp.y;
+        s_matrix[i*blockDim.x+tid*4+2]=temp.z;
+        s_matrix[i*blockDim.x+tid*4+3]=temp.w;
+    }
+    __syncthreads();
+
+    // 存放到shared memory中之后就进行块内规约
+    // 得到最大值，按照图v1.png
+    for(int j=0;j<shared_size;j+=blockDim.x){
+        tmp_max_value[tid]=max(tmp_max_value[tid],s_matrix[j+tid]);
+    }
+    __syncthreads();
+
+
+    // for(int k=BLOCK_SIZE/2;k>0;k/=2){  // BLOCK_SIZE为64
+    //     if(tid<k){
+    //         tmp_max_value[tid]=max(tmp_max_value[tid],tmp_max_value[tid+k]);
+    //     }
+    //     __syncthreads(); // 需要在for内部进行同步，因为下一次循环会用到上一次别的thread的结果
+    // }
+
+    // 使用warp shuffle替代上面的循环，但是由于一个block有64个thread，不能直接进行warp shuffle，所以先对数据进行一次处理，然后再进行warp shuffle
+    if(tid<32){
+        tmp_max_value[tid]=max(tmp_max_value[tid],tmp_max_value[tid+32]);
+    }
+    __syncthreads();
+    for (int offset = 16; offset > 0; offset /= 2) { // warp 内部执行的是同一条指令（只是数据可能不同），一起执行，一起结束，不需要__syncthreads();
+        tmp_max_value[tid]=max(tmp_max_value[tid],__shfl_down_sync(0xffffffff, tmp_max_value[tid], offset));
+    }
+
+
+
+
+    //得到最大的值,然后更新shared memory中的data
+    max_value=tmp_max_value[0];  //得到最大的值
+    for(int l=0;l<shared_size;l+=BLOCK_SIZE){
+        s_matrix[tid+l]=exp(s_matrix[tid+l]-max_value);
+    }
+    __syncthreads();
+
+
+    // 与规约求最大值的方法一致，求sum
+    for(int t=0;t<shared_size;t+=BLOCK_SIZE){
+        tmp_sum_value[tid]=tmp_sum_value[tid]+s_matrix[tid+t];
+    }
+    __syncthreads();
+
+    // for(int t=BLOCK_SIZE/2;t>0;t/=2){
+    //     if(tid<t){
+    //         tmp_sum_value[tid]=tmp_sum_value[tid]+tmp_sum_value[tid+t];
+    //     }
+    //     __syncthreads();
+    // }
+
+    // 使用warp shuffle替代上面的循环
+    tmp_sum_value[tid]=tmp_sum_value[tid]+tmp_sum_value[tid+32];
+    __syncthreads();
+    for(int offset=16;offset>0;offset/=2){
+        tmp_sum_value[tid]=tmp_sum_value[tid]+__shfl_down_sync(0xffffffff,tmp_sum_value[tid],offset);
+    }
+
+    sum_value=tmp_sum_value[0];
+
+    // 下面进行最后的计算,并写回到global memroy
+    for(int i=0;i<shared_size;i+=BLOCK_SIZE){
+        matrix_start[i+tid]=s_matrix[i+tid]/sum_value;
+    }
+}
+
+
+
+int main(){
+    int M=512;
+    int N=512;
+    int matric_size=M*N;
+    float* raw_matrix=(float*)malloc(sizeof(float)*matric_size);
+    float* s_cpu=(float*)malloc(sizeof(float)*matric_size);
+    float* s_gpu=(float*)malloc(sizeof(float)*matric_size);
+
+    float* raw_matrix_device;
+    cudaMalloc((void**)&raw_matrix_device,sizeof(float)*matric_size);
+
+
+    matrix_init(raw_matrix,M,N);
+    softmax_cpu(raw_matrix,M,N);
+
+    constexpr int BLOCK_SIZE=64;
+    dim3 block(BLOCK_SIZE,1);
+    dim3 grid((M+BLOCK_SIZE-1)/BLOCK_SIZE,N);
+
+    constexpr int shared_size=512;
+    softmax_gpu<shared_size,BLOCK_SIZE><<<block,grid>>>(raw_matrix_device,M,N);
+
+    cudaMemcpy(s_gpu,raw_matrix_device,sizeof(float)*matric_size,cudaMemcpyDeviceToHost);
+    compare_matrix(M,N,s_cpu,s_gpu);
+
+    free(raw_matrix);
+    free(s_cpu);
+    free(s_gpu);
+    cudaFree(raw_matrix_device);
+    return 0;
+}
